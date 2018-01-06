b70f0a3fdc7218f2a1ee389fbf9ccdd6b575aacf
Piotr Jalocha <darricck04@gmail.com>
2018-01-06 16:55:02 +0100

    gulp for sass nad es6 without webpack

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+'use strict';
+ 
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    babel = require('gulp-babel');
+
+
+gulp.task('sass', function () {
+  return gulp.src('./src/sass/*.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./app/css'));
+});
+
+gulp.task('gbable', () =>
+    gulp.src('./src/js/*.js')
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(gulp.dest('./app/js/script'))
+);
+
+ 
+gulp.task('work', function () {
+  gulp.watch('./src/sass/*.sass', ['sass']);
+  gulp.watch('./src/js/*.js', ['gbable']);
+});
+
+ 
